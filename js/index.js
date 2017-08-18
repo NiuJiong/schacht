90f3ad2b9e81f22ddbb6c05d990abb95e997f357
lgdcnm <18268815524@163.com>
2017-08-18 09:40:02 +0800

    three

--- js/index.js
+++ js/index.js
@@ -6,4 +6,20 @@ $(function(){
 		$("nav div").click(function(){
 			$("nav ul").slideToggle();
 		});
+		
+	
+//	轮播
+
+	var index = 0;
+	setInterval(function(){
+		$(".banner div").removeClass("active");
+		$(".banner div:eq("+index+")").addClass("active");
+		index++;
+		if(index >= $(".banner div").length){
+			index=0;
+		}
+	},4000);
+	
+	
+	
 });
